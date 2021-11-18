e64889befd99f3aa14a339df61001a41c59f81c9
Ray <74760173+schrodinger95@users.noreply.github.com>
2021-11-17 20:10:11 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -4,12 +4,12 @@ window.addEventListener("load", function() {
 	console.log("Good job opening the window");
 	video = document.querySelector("#player1");
 	video.load()
-	document.querySelector("#volume").innerHTML = video.volume*100 + "%";
 });
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+	document.querySelector("#volume").innerHTML = video.volume*100 + "%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
